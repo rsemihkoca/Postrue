264001b0d89c579854cf906f1e953772012dd2ed
Feyyaz Numan Cavlak <feyyazcavlak@outlook.com>
2023-09-09 22:25:47 +0300

    Add layout

---
+++ frontend/app/_layout.js
@@ -0,0 +1,34 @@
+import { useCallback } from "react";
+import { useFonts } from "expo-font";
+import { Slot } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+SplashScreen.preventAutoHideAsync();
+
+export default function App() {
+  const [fontsLoaded, fontError] = useFonts({
+    "Eudoxus-Black": require("../assets/fonts/EudoxusSans-Bold.ttf"),
+    "Eudoxus-ExtraBold": require("../assets/fonts/EudoxusSans-ExtraBold.ttf"),
+    "Eudoxus-ExtraLight": require("../assets/fonts/EudoxusSans-ExtraLight.ttf"),
+    "Eudoxus-Light": require("../assets/fonts/EudoxusSans-Light.ttf"),
+    "Eudoxus-Medium": require("../assets/fonts/EudoxusSans-Medium.ttf"),
+    "Eudoxus-Regular": require("../assets/fonts/EudoxusSans-Regular.ttf"),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded || fontError) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, fontError]);
+
+  if (!fontsLoaded && !fontError) {
+    return null;
+  }
+
+  return (
+    <SafeAreaView>
+      <Slot name="root" onLayout={onLayoutRootView} />
+    </SafeAreaView>
+  );
+}
--- frontend/app/index.js
+++ frontend/app/index.js
@@ -1,5 +1,11 @@
-import { Text } from "react-native";
+import { Text, View } from "react-native";
+import { StatusBar } from "expo-status-bar";
 
 export default function Page() {
-  return <Text>Home page</Text>;
+  return (
+    <View>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
 }
