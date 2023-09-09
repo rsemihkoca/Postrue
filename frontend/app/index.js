8c7b45451626e5d947b9e2d69eaa96a7c0e78fb4
Bünyamin Korkut <bunyaminkrkt@hotmail.com>
2023-09-09 23:10:04 +0300

    image added

--- frontend/app/index.js
+++ frontend/app/index.js
@@ -1,11 +1,20 @@
-import { Text, View } from "react-native";
+import { Image, StyleSheet, Text, View } from "react-native";
 import { StatusBar } from "expo-status-bar";
+import HomeBottomImage from "../assets/firstPageBottom.png";
 
 export default function Page() {
   return (
-    <View>
+    <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Image style={{ width: '100%', position: 'absolute', bottom: '0px', }} source={HomeBottomImage} />
       <StatusBar style="auto" />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#000',
+  },
+});
