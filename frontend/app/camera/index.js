b74c9a8f919ec81f7a8db84a35678b3ff2a92e27
Feyyaz Numan Cavlak <feyyazcavlak@outlook.com>
2023-09-11 01:38:54 +0300

    Clean vision-camera

--- frontend/app/camera/index.js
+++ frontend/app/camera/index.js
@@ -1,25 +1,11 @@
-import React, { useEffect, useState } from "react";
-import {Camera} from "react-native-vision-camera"
-import { View, Text,StyleSheet } from "react-native";
-import { useCameraDevices } from "react-native-vision-camera";
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
 
 const CameraView = () => {
-  const devices = useCameraDevices();
-
-  if (devices.back != undefined)
-  {
-    const device = devices.back
-    return (
-        <View style={StyleSheet.absoluteFill}>
-          <Camera style={[StyleSheet.absoluteFill, {flex: 1}]} device={device} isActive={true} />
-        </View>
-    );
-  }
-  
-  return ( 
+  return (
     <View>
-        <Text>Kamera Bulunamadı</Text>
-      </View>
+      <Text>Kamera Bulunamadı</Text>
+    </View>
   );
 };
 
