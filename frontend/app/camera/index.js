0723550be11dce1f9662b3eec0bf5dfad6b04c95
rabiaelifcelik <celikr20@itu.edu.tr>
2023-09-10 14:36:05 +0300

    Camera doesn't work - to be fixed

--- frontend/app/camera/index.js
+++ frontend/app/camera/index.js
@@ -1,17 +1,24 @@
 import React from "react";
-import { View, Text } from "react-native";
+import {Camera as Cam} from "react-native-vision-camera"
+import { View, Text,StyleSheet } from "react-native";
 import { useCameraDevices } from "react-native-vision-camera";
 
 const Camera = () => {
+  //const newCameraPermission = Cam.requestCameraPermission()
   const devices = useCameraDevices();
   const device = devices.back;
-
-  if (device == null)
+  console.log("permission status", Cam.getCameraPermissionStatus())
+  //console.log("permission: ", newCameraPermission) //{_h: null}
+  //if (newCameraPermission === null) console.log("permission null")
+  console.log("devices: ", devices)
+  if (device === null)
+  {
     return (
       <View>
         <Text>Kamera Bulunamadı</Text>
       </View>
     );
+  }
   return (
     <Camera style={StyleSheet.absoluteFill} device={device} isActive={true} />
   );
